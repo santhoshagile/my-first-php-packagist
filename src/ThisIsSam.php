a6d789d8e62ffa5e6d2de75e0261c2160bb3cd5f
santhoshagile <santhosha.ganji@agileidc.com>
2025-10-15 12:38:53 +0530

    Second Update

--- src/ThisIsSam.php
+++ src/ThisIsSam.php
@@ -5,6 +5,6 @@ namespace Santhosh\SamLib;
 class ThisIsSam {
 
     function sayHello(){
-        return " Hello I am santosh from new packagist";
+        return " Hello I am santosh from new packagist". rand(0,1000);
     }
 }
